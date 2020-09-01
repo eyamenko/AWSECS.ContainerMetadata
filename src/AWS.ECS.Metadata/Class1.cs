055cd3c418589200515230762b4a58afcc6ad102
Eugene Yamenko <yamenko@me.com>
2020-09-01 19:00:03 +1000

    Get local IPv4

--- src/AWS.ECS.Metadata/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AWS.ECS.Metadata
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/AWS.ECS.Metadata/EcsMetadata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace AWS.ECS.Metadata
+{
+    public static class EcsMetadata
+    {
+        public static IPAddress GetLocalIPv4()
+        {
+            try
+            {
+                var requestUri = new Uri("http://169.254.169.254/latest/meta-data/local-ipv4");
+
+                var request = (HttpWebRequest)WebRequest.Create(requestUri);
+
+                using var response = request.GetResponse();
+                using var stream = response.GetResponseStream();
+                using var reader = new StreamReader(stream);
+
+                return IPAddress.Parse(reader.ReadToEnd());
+            }
+            catch
+            {
+                // Ignore
+            }
+
+            return null;
+        }
+    }
+}

8a30439211ba183de6f6834c66221c434f055771
Eugene Yamenko <yamenko@me.com>
2020-09-01 19:04:39 +1000

    Get public IPv4

--- src/AWS.ECS.Metadata/EcsMetadata.cs
+++ src/AWS.ECS.Metadata/EcsMetadata.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Net;
 
 namespace AWS.ECS.Metadata
 {
     public static class EcsMetadata
     {
-        public static IPAddress GetLocalIPv4()
+        public static IPAddress GetLocalIPv4() => GetIPv4("http://169.254.169.254/latest/meta-data/local-ipv4");
+
+        public static IPAddress GetPublicIPv4() => GetIPv4("http://169.254.169.254/latest/meta-data/public-ipv4");
+
+        private static IPAddress GetIPv4(string requestUriString)
         {
             try
             {
-                var requestUri = new Uri("http://169.254.169.254/latest/meta-data/local-ipv4");
-
-                var request = (HttpWebRequest)WebRequest.Create(requestUri);
+                var request = (HttpWebRequest)WebRequest.Create(requestUriString);
 
                 using var response = request.GetResponse();
                 using var stream = response.GetResponseStream();

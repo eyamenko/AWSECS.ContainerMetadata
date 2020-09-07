b694f152b8f6e49ea0455da5f521c0acca01fe93
Eugene Yamenko <yamenko@me.com>
2020-09-07 15:52:20 +1000

    If network is unreachable, don't log it as an error

--- src/AWSECS.ContainerMetadata/Services/AWSContainerMetadataHttpClient.cs
+++ src/AWSECS.ContainerMetadata/Services/AWSContainerMetadataHttpClient.cs
@@ -17,66 +17,48 @@ namespace AWSECS.ContainerMetadata.Services
 
         public string GetContainerMetadata()
         {
-            try
+            if (Uri.TryCreate(Environment.GetEnvironmentVariable("ECS_CONTAINER_METADATA_URI_V4"), UriKind.Absolute, out var containerMetadataUri))
             {
-                if (Uri.TryCreate(Environment.GetEnvironmentVariable("ECS_CONTAINER_METADATA_URI_V4"), UriKind.Absolute, out var containerMetadataUri))
-                {
-                    return GetResponseString(containerMetadataUri);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Failed to get AWS ECS container metadata.");
+                return GetResponseString(containerMetadataUri);
             }
 
             return null;
         }
 
-        public string GetHostPrivateIPv4Address()
-        {
-            try
-            {
-                return GetResponseString(new Uri("http://169.254.169.254/latest/meta-data/local-ipv4"));
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Failed to get AWS ECS container host's private IPv4 address.");
-            }
+        public string GetHostPrivateIPv4Address() => GetResponseString(new Uri("http://169.254.169.254/latest/meta-data/local-ipv4"));
 
-            return null;
-        }
-
-        public string GetHostPublicIPv4Address()
-        {
-            try
-            {
-                return GetResponseString(new Uri("http://169.254.169.254/latest/meta-data/public-ipv4"));
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Failed to get AWS ECS container host's public IPv4 address.");
-            }
-
-            return null;
-        }
+        public string GetHostPublicIPv4Address() => GetResponseString(new Uri("http://169.254.169.254/latest/meta-data/public-ipv4"));
 
         private string GetResponseString(Uri requestUri)
         {
-            var request = WebRequest.Create(requestUri);
-
-            using var response = (HttpWebResponse)request.GetResponse();
-
-            if (response.StatusCode != HttpStatusCode.OK)
+            try
             {
-                _logger.LogError("Failed to execute HTTP request. Request URI: {RequestUri}, Status code: {StatusCode}.", requestUri, response.StatusCode);
+                var request = WebRequest.Create(requestUri);
 
-                return null;
+                using var response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    _logger.LogError("Failed to execute HTTP request. Request URI: {RequestUri}, Status code: {StatusCode}.", requestUri, response.StatusCode);
+
+                    return null;
+                }
+
+                using var stream = response.GetResponseStream();
+                using var reader = new StreamReader(stream);
+
+                return reader.ReadToEnd();
+            }
+            catch (WebException ex) when (ex.Status == WebExceptionStatus.UnknownError)
+            {
+                // Network is unreachable
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get AWS metadata response.");
             }
 
-            using var stream = response.GetResponseStream();
-            using var reader = new StreamReader(stream);
-
-            return reader.ReadToEnd();
+            return null;
         }
     }
 }
